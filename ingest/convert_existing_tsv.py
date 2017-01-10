6b46a7fda920c01e39859d66e3fe9d7e18410c29
Steve McCauley <steven_mccauley@brown.edu>
2017-01-10 14:28:39 -0500

    Rewrite TSV ingest to handle sources

--- ingest/convert_existing_tsv.py
+++ ingest/convert_existing_tsv.py
@@ -26,10 +26,14 @@ def main(tsvFile):
 			exids= []
 			if row[2] != 'NULL':
 				exids.append( (row[2],'doi') )
+				if row[3] == 'NULL' and row[4] == 'NULL':
+					hrv_exids.add( (row[2], user, 'aa', row[5]) )
 			if row[3] != 'NULL':
 				exids.append( (row[3], 'pmid') )
+				hrv_exids.add( (row[3], user, 'pubmed', row[5]) )
 			if row[4] != 'NULL':
 				exids.append( (row[4], 'wosid') )
+				hrv_exids.add( (row[4], user, 'wos', row[5]) )
 			try:
 				cite_data = json.loads(row[8])
 			except:
@@ -39,26 +43,25 @@ def main(tsvFile):
 				citations.add( (row[0], uri, user) )
 				for tp in exids:
 					cite_exids.add( (row[0], uri, tp[0], tp[1]) )
-			for tp in exids:
-				hrv_exids.add( (row[0], tp[0], user, tp[1], row[5]) )
+				
 
-	with open('users.csv', 'wb') as userFile:
+	with open('data/users.csv', 'wb') as userFile:
 		wrtr = csv.writer(userFile)
 		wrtr.writerows(list(users))
 
-	with open('citations.csv', 'wb') as citeFile:
+	with open('data/citations.csv', 'wb') as citeFile:
 		wrtr = csv.writer(citeFile)
 		wrtr.writerows(list(citations))
 
-	with open('cite_exids.csv', 'wb') as exidFile:
+	with open('data/cite_exids.csv', 'wb') as exidFile:
 		wrtr = csv.writer(exidFile)
 		wrtr.writerows(list(cite_exids))
 
-	with open('hrv_exids.csv', 'wb') as exidFile:
+	with open('data/hrv_exids.csv', 'wb') as exidFile:
 		wrtr = csv.writer(exidFile)
 		wrtr.writerows(list(hrv_exids))
 
-	with open('bad_records.csv', 'wb') as badFile:
+	with open('data/bad_records.csv', 'wb') as badFile:
 		wrtr = csv.writer(badFile)
 		wrtr.writerows(list(bad_json))
 
