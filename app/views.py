2297d4845dc446119628418c9bb3f6a2a6d42a1d
Steve McCauley <steven_mccauley@brown.edu>
2017-01-12 12:09:50 -0500

    Sort and display EXIDs by source

--- app/views.py
+++ app/views.py
@@ -14,8 +14,13 @@ def index():
 @app.route('/rabpubs/<short_id>/pending')
 def harvested_publications(short_id):
 	user = Users.query.filter_by(short_id=short_id).first()
-	# procs = HarvestProcesses.query.filter_by(user_rabid=user.rabid).all()
+	sources = HarvestSources.query.all()
 	exids = HarvestExids.query.filter_by(user_rabid=user.rabid).all()
+	source_map = { source.rabid: []  for source in sources }
+	for exid in exids:
+		source_map[ exid.event.process.source_rabid ].append(exid)
+	source_names = { source.name: source_map[source.rabid]
+						for source in sources }
 	return render_template('pending.html',
 							user=user,
-							exids=exids)	
+							sources=source_names)	
