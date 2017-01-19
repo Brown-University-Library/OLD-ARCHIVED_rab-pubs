777c499abf462bb0e7869fe695b606414b26a86e
Steve McCauley <steven_mccauley@brown.edu>
2017-01-19 16:27:40 -0500

    Timer on Session. Beware of lingering threads

--- app/utils/wos.py
+++ app/utils/wos.py
@@ -1,26 +1,29 @@
 import zeep
 import datetime
+import threading
 
 class Session(object):
 
 	def __init__(self):
 		self.wsdl = 'http://search.webofknowledge.com/esti/wokmws/ws/WOKMWSAuthenticate?wsdl'
 		self.client = zeep.Client(wsdl=self.wsdl)
-		self.last_updated = None
+		self.timer = None
 		self.sid = None
 
 	def authenticate(self):
 		self.sid = self.client.service.authenticate()
-		self.last_updated = datetime.datetime.now()
+		self.timer = threading.Timer( 1200, self.close )
+		self.timer.start()
 
 	def close(self):
 		self.client.service.closeSession()
 
 	def get_sid(self):
 		time_now = datetime.datetime.now()
-		if time_now - self.last_updated < datetime.timedelta(seconds=300):
+		if self.timer.is_alive:
+			self.timer.cancel()
+			self.timer.start()
 			return self.sid
 		else:
-			self.close()
 			self.authenticate()
 			return self.sid
