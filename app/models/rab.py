1101fe3859283835d938b01854086ded024c8d57
Steven McCauley <steven_mccauley@brown.edu>
2017-03-09 15:45:52 -0500

    Refactor RABObject. Listing works, with params

--- app/models/rab.py
+++ app/models/rab.py
@@ -10,163 +10,133 @@ rest_base = app.config['REST_BASE']
 harvest_base = os.path.join(rest_base, 'harvest')
 
 
-
-def RABFactory(rdfType, uri=None, local_name=None, id=None):
-	for cls in RABObject.__subclasses__():
-		if cls.checkRdfType(rdfType):
-			return cls(uri=uri, local_name=local_name, id=id)
-
 class RABObject(object):
 
 	rdf_type = None
 
-	def __init__(self, uri=None, local_name=None, id=None, display=None, data=None):
+	def __init__(self, uri=None, id=None):
 		self.uri_ns = namespaces.RABID
-		self.display = display
-		self.data = data
-		if uri:
+		self.existing = True
+		self.prefix = None
+		self.label = None
+		self.etag = None
+		if id and uri:
+			self.id = id
+			self.uri = uri
+		elif uri and not id:
 			self.uri = uri
 			self.id = uri[len(self.uri_ns):]
-			self.local_name = self.prefix + self.id
-		elif local_name:
-			self.local_name = local_name
-			self.id = local_name[len(self.prefix):]
-			self.uri = self.uri_ns + self.id
-		elif id:
+		elif id and not uri:
 			self.id = id
 			self.uri = self.uri_ns + id
-			self.local_name = self.prefix + id
-		elif id and uri:
-			self.id = id
-			self.uri = uri
-			self.local_name = self.prefix + id
+		else:
+			self.existing = False
 
 	def publish(self):
-		return dict(id=self.id, rabid=self.uri, ns=self.local_name,
-						display=self.display, data=self.data)
+		return dict(id=self.id, rabid=self.uri, display=self.label, data=self.data)
+
+	def load_data(self, data):
+		self.uri = data.keys()[0]
+		self.id = self.uri[len(self.uri_ns):]
+		attrs = data[self.uri]
+		self.label = attrs['label'][0]
+		self.data = attrs
+		self.existing = True
 
 	@classmethod
-	def checkRdfType(cls, rdfType):
-		if rdfType in cls.rdf_type:
-			return True
+	def factory(cls, data):
+		uri = data.keys()[0]
+		rdfType = data[uri]['class']
+		##
+		## Currently, RAB-REST does not return subclass data,
+		## making this ineffective. Returning subclass data will
+		## require Dozer modifications. Revisit if necessary
+		# if cls.__subclasses__():
+		# 	for klass in cls.__subclasses__():
+		# 		if klass.rdf_type == rdfType:
+		# 			rab_obj = klass()
+		# 			rab_obj.load_data(data)
+		# 			return rab_obj
+		# else:
+		if cls.rdf_type == rdfType:
+			rab_obj = cls()
+			rab_obj.load_data(data)
+			return rab_obj
 
 	@classmethod
-	def checkLabel(cls, label):
-		if label == cls.label:
-			return True
-
-
-def HarvestSourceFactory(label, uri=None, local_name=None, id=None):
-	for cls in HarvestSource.__subclasses__():
-		if cls.checkLabel(label):
-			return cls(uri=uri, local_name=local_name, id=id)
-
-class HarvestSource(RABObject):
-
-	rdf_type = [ namespaces.BHARVEST + "HarvestSource" ]
-	label = None
-
-	def __init__(self, uri=None, local_name=None, id=None):
-		self.prefix = prefixes.BHARVEST
-		self.sources_api = os.path.join(harvest_base, 'sources/')
-		self.process_api = os.path.join(harvest_base, 'processes/')
-		super(HarvestSource, self).__init__(uri=uri, local_name=local_name, id=id)
-
-	def exid_lookup(self, exidList):
-		pass
-
-	def list_processes(self, user_rabid):
-		payload = { 'user' : user_rabid, 'source' : self.uri }
-		index_resp = requests.get( self.process_api, params=payload )
-		if index_resp.status_code == 200:
-			data = index_resp.json()
-			queries = []
-			for d in data:
-				proc = d.keys()[0]
-				query_id = namespaces.RABID(query_rabid).id
-				retr_resp = requests.get( rabdata_api + query_id )
-				if retr_resp.status_code == 200:
-					retr_data = retr_resp.json()
-					try:
-						query_data = retr_data[ query_rabid ]
-					except KeyError:
-						raise
-					del query_data['user']
-					del query_data['class']
-					query_data['rabid'] = namespaces.RABID(query_rabid).local_name
-					queries.append(query_data)
-				else:
-					continue
+	def list(cls, params=None):
+		resp = requests.get(cls.rab_api, params=params)
+		if resp.status_code == 200:
+			idx = [] 
+			for data in resp.json():
+				new_rab = cls.factory(data)
+				idx.append(new_rab)
+			return idx
 		else:
-			return 400
-		return jsonify(
-			{ 'params': src.params,'queries': queries })
-
-
-class AcademicAnalytics(HarvestSource):
-
-	label = "Academic Analytics"
-
-	def __init__(self, uri=None, local_name=None, id=None):
-		super(AcademicAnalytics, self).__init__(uri=uri, local_name=local_name, id=id)
-
-
-	def exid_lookup(self, exidList):
-		return dois.get_details(exidList)
-
-	def list_processes(self, user_rabid):
-		return jsonify({ 'params': [],'queries': [] })
-
-class PubMed(HarvestSource):
-
-	label = "PubMed"
-
-	def __init__(self, uri=None, local_name=None, id=None):
-		super(PubMed, self).__init__(uri=uri, local_name=local_name, id=id)
-
-
-	def exid_lookup(self, exidList):
-		return pmids.get_details(exidList)
+			raise
 
 
 wos_session = wos.Session()
 wos_session.authenticate()
 
-class WebOfScience(HarvestSource):
+class HarvestSource(RABObject):
 
-	label = "Web of Science"
+	rdf_type = [ namespaces.BHARVEST + "Source" ]
+	rab_api = os.path.join(harvest_base, 'sources/')
 
-	def __init__(self, uri=None, local_name=None, id=None):
-		super(WebOfScience, self).__init__(uri=uri, local_name=local_name, id=id)
+	def __init__(self, uri=None, id=None):
+		self.prefix = prefixes.BHARVEST
+		self.process_api = os.path.join(harvest_base, 'processes/')
+		super(HarvestSource, self).__init__(uri=uri, id=id)
 
 	def exid_lookup(self, exidList):
-		sid = wos_session.get_sid()
-		return wosids.get_details(exidList, sid)
+		if self.label == 'Academic Analytics':
+			return dois.get_details(exidList)
+		elif self.label == 'PubMed':
+			return pmids.get_details(exidList)
+		elif self.label == 'Web of Science':
+			sid = wos_session.get_sid()
+			return wosids.get_details(exidList, sid)
 
-
-
-def HarvestProcessFactory(rdfType=None, uri=None, local_name=None, id=None):
-	if rdfType == None:
-		if id:
-			uri = namespaces.RABID + id 
-			proc = local.HarvestProcesses.query.filter_by(rabid=uri).first()
-			rdfType = proc.rabclass
-		elif uri:
-			proc = local.HarvestProcesses.query.filter_by(rabid=uri).first()
-			rdfType = proc.rabclass
-	for cls in HarvestProcess.__subclasses__():
-		if cls.checkRdfType(rdfType):
-			return cls(uri=uri, local_name=local_name, id=id)
+	def list_processes(self, user_rabid):
+		if self.label == 'Academic Analytics':
+			return jsonify({ 'params': [],'queries': [] })
+		else:
+			payload = { 'user' : user_rabid, 'source' : self.uri }
+			index_resp = requests.get( self.process_api, params=payload )
+			if index_resp.status_code == 200:
+				data = index_resp.json()
+				queries = []
+				for d in data:
+					proc = d.keys()[0]
+					query_id = namespaces.RABID(query_rabid).id
+					retr_resp = requests.get( rabdata_api + query_id )
+					if retr_resp.status_code == 200:
+						retr_data = retr_resp.json()
+						try:
+							query_data = retr_data[ query_rabid ]
+						except KeyError:
+							raise
+						del query_data['user']
+						del query_data['class']
+						query_data['rabid'] = namespaces.RABID(query_rabid).local_name
+						queries.append(query_data)
+					else:
+						continue
+			else:
+				return 400
+			return jsonify(
+				{ 'params': src.params,'queries': queries })
 	
 
 class HarvestProcess(RABObject):
 
 	rdf_type = [ namespaces.BHARVEST + "HarvestProcess" ]
+	rab_api = os.path.join(harvest_base, 'processes/')
 
-	def __init__(self, uri=None, local_name=None, id=None):
-		self.rab_api = os.path.join(harvest_base, 'processes/')
+	def __init__(self, uri=None, id=None):
 		self.prefix = prefixes.BHARVEST
-		super(HarvestProcess, self).__init__(uri=uri, local_name=local_name, id=id)
+		super(HarvestProcess, self).__init__(uri=uri, id=id)
 
 	def create(self, user_rabid=None, src_rabid=None, data=None):
 		for k, v in data.items():
@@ -212,20 +182,20 @@ class WebOfScienceSearch(HarvestProcess):
 	rdf_type = [	namespaces.BHARVEST + "HarvestProcess",
 					namespaces.BHARVEST + "WebOfScienceSearch" ]
 
-	def __init__(self, uri=None, local_name=None, id=None):
+	def __init__(self, uri=None, id=None):
 		self.params = 	[	'Topic','Title','Author','Author Identifiers',
 							'Group Author','Editor','Publication Name',
 							'DOI','Year Published','Address',
 							'Organizations-Enhanced','Conference',
 							'Language','Document Type','Funding Agency',
 							'Grant Number','Accession Number','PubMed ID']
-		super(WebOfScienceSearch, self).__init__(uri=uri, local_name=local_name, id=id)
+		super(WebOfScienceSearch, self).__init__(uri=uri, id=id)
 
 class PubMedSearch(HarvestProcess):
 
 	rdf_type = [	namespaces.BHARVEST + "HarvestProcess",
 					namespaces.BHARVEST + "PubMedSearch" ]
-	def __init__(self, uri=None, local_name=None, id=None):
+	def __init__(self, uri=None, id=None):
 		self.params = [	'Title/Abstract','Author - Last','ISBN',
 						'MeSH Terms','Affiliation','Author - Full',
 						'Date - MeSH','Location ID','Publication Type',
@@ -242,14 +212,14 @@ class PubMedSearch(HarvestProcess):
 						'Pharmacological Action','Filter',
 						'Date - Create','Author - Identifier',
 						'Author - Corporate']
-		super(PubMedSearch, self).__init__(uri=uri, local_name=local_name, id=id)
+		super(PubMedSearch, self).__init__(uri=uri, id=id)
 
 class AcademicAnalyticsUpload(HarvestProcess):
 
 	rdf_type = [	namespaces.BHARVEST + "HarvestProcess",
 			namespaces.BHARVEST + "AcademicAnalyticsUpload" ]
 
-	def __init__(self, uri=None, local_name=None, id=None):
+	def __init__(self, uri=None, id=None):
 		self.params = []
-		super(AcademicAnalyticsUpload, self).__init__(uri=uri, local_name=local_name, id=id)
+		super(AcademicAnalyticsUpload, self).__init__(uri=uri, id=id)
 
